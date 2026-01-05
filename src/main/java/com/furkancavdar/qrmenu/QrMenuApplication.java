79e2dedfcc3de9318864ac403df227a33bb70e7f
Furkan Cavdar <m.furkancavdar@gmail.com>
2026-01-05 07:12:04 +0100

    refactor: update environment configuration and clean up application settings

--- src/main/java/com/furkancavdar/qrmenu/QrMenuApplication.java
+++ src/main/java/com/furkancavdar/qrmenu/QrMenuApplication.java
@@ -5,9 +5,7 @@ import com.networknt.schema.SpecVersion;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
@@ -21,10 +19,5 @@ public class QrMenuApplication {
   @Bean
   public JsonSchemaFactory jsonSchemaFactory() {
     return JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
-  }
-
-  @EventListener(ApplicationReadyEvent.class)
-  public void checkVirtualThreads() {
-    log.info("Virtual threads enabled: {}", Thread.currentThread().isVirtual());
   }
 }
